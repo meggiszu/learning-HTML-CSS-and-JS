7f35025e2fe9cb88c408c17b47e618c67d10442c
Magdalena Szula-Chraplewska <magdalenaszula@gmail.com>
2017-12-09 22:28:13 +0100

    one clicked

--- lottery/script.js
+++ lottery/script.js
@@ -1,32 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
+  const result = [];
+  let clicked = false
 
-const result = [];
+  const losowanie = function() {
+    if (!clicked) {
+      const container = document.getElementById('container');
+      if (result.length === 10) return
 
-const losowanie = function() {
-  const container = document.getElementById('container');
+      const wynikLosowania = Math.floor(Math.random() * 99 + 1);
+      for (let i = 0 ; i < result.length; i++) {
+      
+        if (wynikLosowania === result[i]) {
+          return losowanie()
+        }
+      }
+       
+      const div = document.createElement('div');
+      div.setAttribute('class', 'ball');
+  
+      div.textContent = wynikLosowania;
+      result.push(wynikLosowania);
+      container.appendChild(div);
 
-  if (result.length === 10) return;
-
-  const wynikLosowania = Math.floor(Math.random() * 99 + 1);
-
-    for (let i = 0 ; i < result.length; i++) {
-    
-      if (wynikLosowania === result[i]) {
-      return losowanie()
+      clicked = true
+    } else {
+      return false;
     }
   }
-   
-  const div = document.createElement('div');
-  div.setAttribute('class', 'ball');
-
-
-  div.textContent = wynikLosowania;
-
-  result.push(wynikLosowania);
-  
-  container.appendChild(div);
-}
-const button = document.querySelector("button");
-
-button.addEventListener("click",losowanie);
+    
+  const button = document.querySelector("button");
+  button.addEventListener("click",losowanie);
 });
