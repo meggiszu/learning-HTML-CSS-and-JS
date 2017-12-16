c0efcb9dd2f4588a979aa5c8ccf1eedbb3ff568c
Magdalena Szuła-Chraplewska <magdalenaszula@gmail.com>
2017-12-17 00:08:17 +0100

    blue dots added

--- simplePaint/script.js
+++ simplePaint/script.js
@@ -2,15 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   let active = false;
+  let counter = 0
 
   const draw = function(e) {
     if(active == false) return;
+
     const x = e.clientX;
     const y = e.clientY;
     const div = document.createElement('div');
     div.style.top = y + "px";
     div.style.left = x + "px";
+
+    if(counter > 10) {
+      div.style.backgroundColor = "blue";
+    };
+
     document.body.appendChild(div);
+    counter++
   };
 
 
