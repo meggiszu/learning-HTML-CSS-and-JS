0a17a3b068c71ac6af245875d6481a21c175abce
Magdalena Szuła-Chraplewska <magdalenaszula@gmail.com>
2017-12-16 23:22:08 +0100

    simple paint based on divs added

---
+++ simplePaint/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+  let active = false;
+
+  const draw = function(e) {
+    if(active == false) return;
+    const x = e.clientX;
+    const y = e.clientY;
+    const div = document.createElement('div');
+    div.style.top = y + "px";
+    div.style.left = x + "px";
+    document.body.appendChild(div);
+  };
+
+
+   const drawActive = function() {
+      active = !active
+  };
+
+
+    document.body.addEventListener("mousemove", draw);
+    document.body.addEventListener("mousedown", drawActive)
+    document.body.addEventListener("mouseup", drawActive)
+});
