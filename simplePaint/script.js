1355ff81f37c3aa9095fdf5fe58c5374fc9ec514
Magdalena Szuła-Chraplewska <magdalenaszula@gmail.com>
2017-12-17 12:15:06 +0100

    Math.random, drag function  added

--- simplePaint/script.js
+++ simplePaint/script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   let active = false;
-  let counter = 0
+  let counter = 0;
+  let currentColor = "pink";
+  let colors = ["#DC4C46", "#672E3B", "#223A5E","#898E8C","#005960","#9C9A40"];
 
   const draw = function(e) {
     if(active == false) return;
@@ -12,22 +14,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const div = document.createElement('div');
     div.style.top = y + "px";
     div.style.left = x + "px";
-
-    if(counter > 10) {
-      div.style.backgroundColor = "blue";
-    };
-
+    
+    if (counter >= 20) {
+      currentColor = colors[Math.floor(Math.random() * 10)%6]
+      counter = 0 
+    }
+  
+    div.style.backgroundColor = currentColor
     document.body.appendChild(div);
     counter++
   };
 
 
-   const drawActive = function() {
+   const toggleActive= function() {
       active = !active
-  };
+   };
 
 
     document.body.addEventListener("mousemove", draw);
-    document.body.addEventListener("mousedown", drawActive)
-    document.body.addEventListener("mouseup", drawActive)
+    document.body.addEventListener("mousedown", toggleActive);
+    document.body.addEventListener("mouseup", toggleActive);
+    document.body.addEventListener("drag", function() { active = false })
 });
+
+
