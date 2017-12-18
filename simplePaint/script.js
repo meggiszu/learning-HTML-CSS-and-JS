e38aba736f3464e3902fa95ffe2a709c70e04d08
Magdalena Szuła-Chraplewska <magdalenaszula@gmail.com>
2017-12-18 23:35:27 +0100

    counting panel added

--- simplePaint/script.js
+++ simplePaint/script.js
@@ -1,40 +1,64 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-
   let active = false;
   let counter = 0;
-  let currentColor = "pink";
-  let colors = ["#DC4C46", "#672E3B", "#223A5E","#898E8C","#005960","#9C9A40"];
+  let colors = [
+    {color:"#DC4C46", name: "Grenadine", counter: 0}, 
+    {color:"#672E3B", name: "Tawny Port", counter: 0}, 
+    {color:"#898E8C", name: "Neutral Gray", counter: 0},
+    {color:"#4F84C4", name: "Marina", counter: 0},
+    {color:"#005960", name: "Shaded Spruce", counter: 0},
+    {color:"#9C9A40", name: "Golden Lime", counter: 0},
+  ];
+
+  let currentColor = getColor()
+  updateCounter(currentColor)
+
+  const paint = document.getElementById('paint')
+
+  function getColor() {
+    return colors[Math.floor(Math.random() * 10)%6]
+  }
+
+  function updateCounter(col) {
+    col['counter']++
+    document.getElementById(col['color']).innerHTML = col['counter']
+  }
 
   const draw = function(e) {
-    if(active == false) return;
+    if(active == false) {
+      return;
+    } 
 
     const x = e.clientX;
     const y = e.clientY;
     const div = document.createElement('div');
     div.style.top = y + "px";
     div.style.left = x + "px";
+    div.setAttribute('class', 'dot')
     
     if (counter >= 20) {
-      currentColor = colors[Math.floor(Math.random() * 10)%6]
+
+      const col = getColor()
+      updateCounter(col)
+      currentColor = col
+
       counter = 0 
     }
   
-    div.style.backgroundColor = currentColor
-    document.body.appendChild(div);
+    div.style.backgroundColor = currentColor['color']
+    paint.appendChild(div);
     counter++
   };
 
 
-   const toggleActive= function() {
-      active = !active
-   };
+  const toggleActive= function() {
+    active = !active
+  };
 
-
-    document.body.addEventListener("mousemove", draw);
-    document.body.addEventListener("mousedown", toggleActive);
-    document.body.addEventListener("mouseup", toggleActive);
-    document.body.addEventListener("drag", function() { active = false })
+  paint.addEventListener("mousemove", draw);
+  paint.addEventListener("mousedown", toggleActive);
+  paint.addEventListener("mouseup", toggleActive);
+  paint.addEventListener("drag", function() { active = false });
 });
 
 
